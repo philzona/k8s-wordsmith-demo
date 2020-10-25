f373a59ab7be21a7b1e8727dc52845ddd6b27c38
Phil Zona <phil.b.zona@gmail.com>
2020-10-25 12:32:15 -0500

    Add integration test script

---
+++ test/integration.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export HTTP_PASS
+
+external_ip=$(kubectl get svc -n $NS | grep web | awk '{print $4}')
+port_string=$(kubectl get svc -n $NS | grep web | awk '{print $5}')
+port=$(echo $port_string | cut -d ":" -f 1)
+
+status=$(curl -sL -o /dev/null -w "%{http_code}" "http://${external_ip}:${port}")
+
+if [ $status -eq 200 ]; then
+    HTTP_PASS="true"
+else
+    HTTP_PASS="false"
+fi;
+
+cf_export HTTP_PASS
