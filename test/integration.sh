048a343743c34d1bd52545f1410478c31c8ff3b3
Phil Zona <phil.b.zona@gmail.com>
2020-10-25 13:45:50 -0500

    More test improvements

--- test/integration.sh
+++ test/integration.sh
@@ -6,11 +6,18 @@ echo "Getting external IP address for web service..."
 external_ip=$(kubectl get svc -n $NS | grep web | awk '{print $4}')
 echo "External IP: $external_ip"
 
-# Retry until the service has not been fully provisioned
+count=0
+# Retry until the service has not been fully provisioned or for 2 minutes
 while [ $external_ip = '<pending>' ]; do
     sleep 5
     external_ip=$(kubectl get svc -n $NS | grep web | awk '{print $4}')
     echo "External IP: $external_ip"
+    $count++
+
+    if [ $count -ge 24 ]; then
+        echo "No response after 2 minutes, something is probably wrong"
+        exit 1
+    fi;
 done;
 
 echo "Getting port..."
@@ -22,6 +29,7 @@ status=$(curl -sL -o /dev/null -w "%{http_code}" "http://${external_ip}:${port}"
 
 if [ $status -eq 200 ]; then
     HTTP_PASS="true"
+    exit 0
 else
     HTTP_PASS="false"
 fi;
@@ -33,3 +41,9 @@ echo "Request status: $status"
 echo "Test passed: $HTTP_PASS"
 
 cf_export HTTP_PASS
+
+if [ $HTTP_PASS = "true" ]; then
+    exit 0
+else
+    exit 1
+fi;
