038e78c7328e9a90187b6fc1ddacfc19ce24c813
Phil Zona <phil.b.zona@gmail.com>
2020-10-25 12:48:55 -0500

    Add retry loop for getting external ip

--- test/integration.sh
+++ test/integration.sh
@@ -3,9 +3,15 @@
 export HTTP_PASS
 
 external_ip=$(kubectl get svc -n $NS | grep web | awk '{print $4}')
+
+# Retry until the service has not been fully provisioned
+while [ external_ip = '<pending>' ]; do
+    sleep 10
+    external_ip=$(kubectl get svc -n $NS | grep web | awk '{print $4}')
+done;
+
 port_string=$(kubectl get svc -n $NS | grep web | awk '{print $5}')
 port=$(echo $port_string | cut -d ":" -f 1)
-
 status=$(curl -sL -o /dev/null -w "%{http_code}" "http://${external_ip}:${port}")
 
 if [ $status -eq 200 ]; then
